f5534138eacff0f8b1ea48565ee69ea19175917f
Iverance <jeremhh@gmail.com>
2014-05-12 07:46:02 -0700

    prototype

---
+++ db.js
@@ -0,0 +1,53 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/chat', function(err){
+	if(err) {
+		console.log(err);
+	} else {
+		console.log('Connected to mongodb!');
+	}
+});
+
+var signSchema = mongoose.Schema({
+	user: String,
+	passwd: String,
+	created: {type: Date, default: Date.now}
+});
+
+var chatSchema = mongoose.Schema({
+	nick: String,
+	msg: String,
+	created: {type: Date, default: Date.now}
+});
+
+var Chat = mongoose.model('Message', chatSchema);
+
+var SignUp = mongoose.model('User', signSchema);
+
+exports.login = function(data, cb){
+	var query = SignUp.findOne(data);
+	query.exec(function(err, docs){
+		cb(err, docs);
+	});
+}
+
+exports.signUpNew = function(data, cb){
+	var newUser = new SignUp({user: data.user, passwd: data.passwd});
+	newUser.save(function(err){
+		cb(err);
+	});
+}
+
+exports.getOldMsgs = function(limit, cb){
+	var query = Chat.find({});
+	query.sort('-created').limit(limit).exec(function(err, docs){
+		cb(err, docs);
+	});
+}
+
+exports.saveMsg = function(data, cb){
+	var newMsg = new Chat({msg: data.msg, nick: data.nick});
+	newMsg.save(function(err){
+		cb(err);
+	});
+};
---
+++ public/js/home.js
@@ -0,0 +1,2 @@
+var signName = $("#signName"),
+	signpw = $("#signpw")
